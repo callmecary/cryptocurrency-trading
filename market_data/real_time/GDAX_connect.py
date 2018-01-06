f187a21b09dd5a5c6e625413464a9fded84ef681
caiqifang <cai2053@gmail.com>
2018-01-06 18:08:03 -0500

    Now able to fetch account information from GDax example

--- market_data/real_time/GDAX_connect.py
+++ market_data/real_time/GDAX_connect.py
@@ -1,4 +1,7 @@
 import asyncio, websockets
+import json, hmac, hashlib, time, requests, base64
+from requests.auth import AuthBase
+from account import Account
 
 @asyncio.coroutine
 def hello():
@@ -10,20 +13,42 @@ def hello():
     finally:
         yield from websocket.close()
 
-asyncio.get_event_loop().run_until_complete(hello())
 
-"""
-ws = websocket.WebSocket()
-#ws.connect("wss://ws-feed.gdax.com")
+# Create custom authentication for Exchange
+class CoinbaseExchangeAuth(AuthBase):
+    def __init__(self, api_key, secret_key, passphrase):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.passphrase = passphrase
 
-send_string = json.loads('{"type": "subscribe", "product_ids": ["BTC-USD"], "channels": '
-                         '["level2", "heartbeat", {"name": "ticker","product_ids": ["BTC-USD"]},]}')
+    def __call__(self, request):
+        timestamp = str(time.time())
+        message = timestamp + request.method+request.path_url+(request.body or '')
+        message = message.encode('ascii')
+        hmac_key = base64.b64decode(self.secret_key)
+        signature = hmac.new(hmac_key, message, hashlib.sha256)
+        signature_b64 = base64.b64encode(signature.digest()).decode('utf-8')
 
-print send_string
-#ws.send(send_string)
-print("Sent")
-print("Receiving...")
-#result = ws.recv()
-print("Received '%s'" % result)
-#ws.close()
-"""
+        request.headers.update({
+            'CB-ACCESS-SIGN': signature_b64,
+            'CB-ACCESS-TIMESTAMP': timestamp,
+            'CB-ACCESS-KEY': self.api_key,
+            'CB-ACCESS-PASSPHRASE': self.passphrase,
+            'Content-Type': 'application/json'
+        })
+        return request
+
+def main():
+    api_url = 'https://api.gdax.com/'
+    account = Account()
+    auth = CoinbaseExchangeAuth(account.API_KEY,
+                                account.API_SECRET,
+                                account.API_PASS)
+
+    # Get accounts
+    r = requests.get(api_url + 'accounts', auth=auth)
+    print(r.json())
+    #asyncio.get_event_loop().run_until_complete(hello())
+
+if __name__ == '__main__':
+    main()
